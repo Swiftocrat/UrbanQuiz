d1253fa27abbcb7ffc1da86aa31d942255c196a3
Yaroslav Vyshniak <yaroslavvushnyak@gmail.com>
2021-05-18 18:04:57 +0300

    Initial Commit

---
+++ UrbanQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UrbanQuiz
+//
+//  Created by Yaroslav Vyshniak on 18.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UrbanQuiz/UrbanQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanQuizApp.swift
+//  UrbanQuiz
+//
+//  Created by Yaroslav Vyshniak on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct UrbanQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
