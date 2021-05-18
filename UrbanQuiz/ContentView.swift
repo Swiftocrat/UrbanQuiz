4861be5d645ad8b453461ec22b4e285dac038736
Yaroslav Vyshniak <yaroslavvushnyak@gmail.com>
2021-05-18 18:38:23 +0300

    Initial commit

--- UrbanQuiz/ContentView.swift
+++ UrbanQuiz/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  var answers = ["Ні, не може бути","У випадку, коли метро задороге", "У випадку, коли грунт поганий","Так, може бути завжди"]
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+      VStack(alignment: .center) {
+        Image("tram")
+          .resizable()
+          .frame(width: 300, height: 170, alignment: .top)
+          .padding(.top, -10)
+          .scaledToFill()
+        Text("Чи трамвай може бути альтернативою метро?")
+          .bold()
+        //Spacer(minLength: 5)
+        ForEach(answers, id: \.self) { answer in
+          Button(answer) {
+            print("some shit")
+          }
+          .padding()
+          .background(Color.orange)
+          .foregroundColor(.black)
+          .cornerRadius(15)
+
+        }
+      }
+        
     }
 }
 
